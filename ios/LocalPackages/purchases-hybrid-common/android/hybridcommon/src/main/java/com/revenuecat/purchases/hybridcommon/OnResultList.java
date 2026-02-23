d0f64f121048a8ace358c760ca7a693152c23655
Maicon <djmkapple@gmail.com>
2026-02-23 18:23:17 -0300

    ios: vendor local SPM packages for Xcode Cloud (trimmed)

--- ios/LocalPackages/purchases-hybrid-common/android/hybridcommon/src/main/java/com/revenuecat/purchases/hybridcommon/OnResultList.java
+++
@@ -1,9 +0,0 @@
-package com.revenuecat.purchases.hybridcommon;
-
-import java.util.List;
-import java.util.Map;
-
-public interface OnResultList {
-    void onReceived(List<Map<String, ?>> map);
-    void onError(ErrorContainer errorContainer);
-}
